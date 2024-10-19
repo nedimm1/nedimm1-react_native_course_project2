f11686066f130c69bdd8019a916e94be34df328e
Nedim Muminovic <nedimmuminovic61@gmail.com>
2024-10-19 18:03:54 +0200

    creating styles with shadow

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,31 @@
-import {View, TextInput} from 'react-native'
+import {View, TextInput, StyleSheet} from 'react-native'
 
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
   return (
-   <View>
+   <View styles={styles.inputContainer}>
     <TextInput></TextInput>
     <PrimaryButton>reset</PrimaryButton>
     <PrimaryButton>confirm</PrimaryButton>
    </View>
   )
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      marginTop: 100,
+      marginHorizontal: 24,
+      padding: 16,
+      backgroundColor: '#72063c',
+      borderRadius: 8,
+      elevation: 4,
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 6,
+      shadowOpacity: 0.25,
+    },
+  });
+  
 
 export default StartGameScreen;
