ff6430a1d83f80be70a322bd4fb951582d1000e6
Nedim Muminovic <nedimmuminovic61@gmail.com>
2024-10-23 14:08:49 +0200

    adding some things to the button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,31 @@
-import {View, Text} from 'react-native'
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
-function PrimaryButton({children}) {
+function PrimaryButton({ children }) {
+
+ function handlePress(){
+  console.log("pressed")
+ }
+
   return (
-   <View>
-    <Text>{children}</Text>
-   </View>
-  )
+    <View style={styles.buttonContainer}>
+      <Pressable onPress={handlePress}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
-export default PrimaryButton
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: "#8c0046",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
+
+export default PrimaryButton;
