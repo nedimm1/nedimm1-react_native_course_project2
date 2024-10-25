8ba85de54659aeef3816c5fc8fbf263d2d89893f
Nedim Muminovic <nedimmuminovic61@gmail.com>
2024-10-25 15:48:05 +0200

    adding some stuff to the GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,24 @@
-import { View } from "react-native"
+import { View, Text, StyleSheet } from 'react-native';
 
 function GameScreen() {
   return (
-    <View>
-
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      <View>LOG ROUNDS</View>
     </View>
-  )
+  );
 }
 
-export default GameScreen
+export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12
+    }
+});
